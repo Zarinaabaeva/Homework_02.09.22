42894b609c314f53d3ee0226d58ea2eb21631d6f
Zarina <abaevazarina@yandex.ru>
2022-09-03 02:17:50 +0300

    added task 15

---
+++ task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a == 6 || a == 7){
+    Console.WriteLine("выходной");
+}
+else if(a < 1 || a > 7){
+    Console.WriteLine("это не день недели");
+}   
+else{
+    Console.WriteLine("не выходной");
+}     
