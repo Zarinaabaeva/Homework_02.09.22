c4f53e7efc473e0636ae3e79a812cfdb34d06ed6
Zarina <abaevazarina@yandex.ru>
2022-09-03 02:12:48 +0300

    added task 13

---
+++ task_13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int x = a / 100 % 10;
+if(a / 100 > 0){
+    Console.WriteLine(x);
+}
+else{
+    Console.WriteLine("третьей цифры нет");
+}
+
